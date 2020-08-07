7271abda062dcc4b6218a18ea0c5886df30bcf01
caborgees <camilabfotografia@gmail.com>
2020-08-07 16:20:11 -0300

    First Commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//
+function back(){
+document.getElementById("img-menu").src="images/image 18.png"
+document.getElementById("img-name").src="images/penne.png"}
+
+function next(){
+  document.getElementById("img-menu").src="images/image 19.png"
+  document.getElementById("img-name").src="images/pannacota.png"}
